fcf2efa3939c7059e3a49aa880daa43602a0ccac
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-11 22:44:00 +0000

    0x01.C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,17 @@
  *main - Entry point
  *
  *Description: This program prints all letters from a-z in lower case
+ *Return: Returns 0 upon successful completion
  */
 int main(void)
 {
-char l = "a";
-for (; l <= "z"; l++)
+char letter = 'a';
+
+while (letter <= 'z')
 {
-putchar (l, " ");
+putchar(letter);
+letter++;
 }
+putchar('\n');
 return (0);
 }
