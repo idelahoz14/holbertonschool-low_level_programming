b8029ddb6ed13cc8247a1ecae594747b568d95c0
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-12 01:22:33 +0000

    0x01.C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: This program will print all letters in lowercase except for
+ *q and e.
+ *Return: 0 upon successful completion
+ */
+int main(void)
+{
+char l = "a";
+for (l; l < z; l++)
+{
+if (l != "q" || l != "e")
+{
+putchar(l);
+}
+}
+putchar("\n")
+return (0)
+}
