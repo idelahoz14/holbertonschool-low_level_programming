68aa237415607fa71ae630d84eeb161a5020b8e3
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-12 03:46:10 +0000

    0x01.C

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *
+ *Description: Function will print all single digit numbers in a comma
+ *separated list without the use of any char variables.
+ *Return: 0 upon successful completion
+ */
+int main(void)
+{
+int num = '0';
+
+while (num <= '9')
+{
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+return (0);
+}
