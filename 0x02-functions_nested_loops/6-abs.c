ca9782762ed2949cf1b7d24b3db15a1c840d089f
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-15 22:34:47 +0000

    0x02.C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *Description: This program will return the absolute value of a number
+ *
+ *Return: Always 0
+ */
+int _abs(int)
+{
+if (n <= 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
