629961bb0ef4a5e0863dea34c887e7c030ea71ea
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-15 20:28:39 +0000

    0x02.C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_alphabetx10 - prints the letters from a to z in lowercase x10
+ *
+ *Return: Always 0
+ */
+void main(void)
+{
+int num = '0';
+
+do{
+int letter = 'a';
+while (letter <= z){
+_putchar(letter);
+letter++;
+}
+num++;
+_putchar('\n');
+}while (num < '10');
+}
