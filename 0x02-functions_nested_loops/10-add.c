d87ab9b48b131884464e15eed5e00cb9aa542202
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-16 21:41:02 +0000

    0x02.C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *add - Adds two integers
+ *@num1: tested variable
+ *@num2: tested variable
+ *
+ *Description: Adds num1 and num2 and returns the sum
+ *Return: returns the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+  return (num1 + num2);
+}
