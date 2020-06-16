58d47a045bc50eee6de478dcbad2f262cf7ac319
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-16 16:39:19 +0000

    0x02.C

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
  *_isalpha - decides if the character is a letter
+ *@c: character to be tested
  *
  *Description: This program will check if the letter is lowercase or uppercase
  *Return: 1 if lowercase or uppercase letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-if ((c >= 'A' && c <= 'Z') || (c <= 'a' && c >= 'z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
