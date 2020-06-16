792486dcac62b54c04b64339ab9d500434f42120
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-16 17:26:52 +0000

    0x02.C

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
+ *_abs - Returns the absolute value of n
  *Description: This program will return the absolute value of a number
- *
+ *@n: variable tested
  *Return: the absolute value of an integer.
  */
-int _abs(int)
+int _abs(int n)
 {
 if (n <= 0)
 {
