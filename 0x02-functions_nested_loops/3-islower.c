6883f7bf14c9aa4e34e9e51d544f701f6c951822
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-16 17:07:06 +0000

    0x02.C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  *_islower - Decides if a character is lowercase letter
- *
+ *@c: tested variable
  *Description: This function returns 1 if the character is lowercase
  *and 0 if the character is not lowercase
  *Return: 1 if lowercase, 0 if not
