96120080e6512e387232a6aabf61be54308e3a74
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-19 01:44:10 +0000

    0x04.C

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_square - Prints a square followed by a new line
+ *@size: test variable
+ *
+ *Return: void
+ */
+void print_square(int size)
+{
+int index = 0;
+do
+{
+int count = 0;
+while(count < size)
+{
+putchar('#');
+count++;
+}
+putchar('\n');
+index++;
+}while(index < size);
+}
