ec7cf1e60467c70172e91fc871aedd82f0245f3d
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-19 03:25:20 +0000

    0x04.C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int number;
 
 for (number = 1; number <= 100; number++)
 {
-if ((number % 3 == 0) && (number % 5 != 0))
+if ((number % 3 == 0) && (number % 5 == 0))
 {
 printf("FizzBuzz");
 }
-else if (number % 5 == 0)
+else if ((number % 5 == 0) && (number % 3 != 0))
 {
 printf("Buzz");
 }
-else if (number % 3 == 0)
+else if ((number % 3 == 0) && (number % 5 == 0))
 {
 printf("Fizz");
 }
