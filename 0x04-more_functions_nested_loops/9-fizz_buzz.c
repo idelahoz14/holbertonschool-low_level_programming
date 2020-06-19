180a77a0489f240bbdcb0d2daff9445ab3588816
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-19 02:54:14 +0000

    0x04.C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,21 @@ int number = 1;
 while (number <= 100)
 {
 if ((number % 3) == 0 && (number % 5) == 0)
+{
 printf("FizzBuzz ");
+}
 else if ((number % 3) == 0 && (number % 5) != 0)
+{
 printf("Fizz ");
+}
 else if ((number % 3) != 0 && (number % 5) == 0)
+{
 printf("Buzz ");
+{
 else
+{
 printf("%d ", number);
+}
 if (number != 100)
 {
 number++;
