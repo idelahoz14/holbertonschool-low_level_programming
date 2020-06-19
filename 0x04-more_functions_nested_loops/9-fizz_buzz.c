cd71b7c622cc52afe0fdb8cabdeea2ed14bb52c8
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-19 02:43:43 +0000

    0x04.C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: This program will print the numbers between 1 and 100
+ *followed by a new line. For multiples of 3, the program will print
+ *Fizz instead of the number. For multiples of 5, it will print Buzz.
+ *If the numbers are multiples of both, it prints FizzBuzz.
+ *Return: 0 upon successful completion
+ */
+int main(void)
+{
+int number = 1;
+
+while (number <= 100 && number != 100)
+{
+if ((number % 3) == 0 && (number % 5) == 0)
+printf("FizzBuzz ");
+else if ((number % 3) == 0 && (number % 5) != 0)
+printf("Fizz ");
+else if ((number % 3) != 0 && (number % 5) == 0)
+printf("Buzz ");
+else
+printf("%d ", num);
+num++;
+printf("\n");
+return (0);
+}
