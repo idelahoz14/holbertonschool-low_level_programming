dcce70e563899e1e403f7f5f1fd8325630243f07
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-23 19:34:02 +0000

    0x05.C

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *rev_string - reverses a string
+ *@s: test variable
+ *
+ *Description: Reverses a string directly at its pointer address
+ *Return: void
+ */
+void rev_string(char *s)
+{
+int count = 0;
+
+while (*s != '\0')
+{
+_putchar(*s);
+count++;
+s++;
+}
+_putchar('\n');
+s--;
+while (count != 0)
+{
+putchar(*s);
+s--;
+count--;
+}
+_putchar('\n');
+}
