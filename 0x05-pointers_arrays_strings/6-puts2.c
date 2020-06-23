a48dca4020661b49c1ca23c10293d2d072815ba4
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-23 19:58:04 +0000

    0x05.C

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *puts2 - prints every 2 chars of a string
+ *@str: test variable
+ *
+ *Return: void
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+if ((*str % 2) == 0)
+{
+putchar(*str);
+}
+str++;
+}
+putchar('\n');
+}
