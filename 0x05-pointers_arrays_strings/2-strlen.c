69420030ddd11824ecfdc46a4e7341d54ee66fe7
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-23 15:23:14 +0000

    0x05.C

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: test variable
+ *
+ *Return: length of string s
+ */
+int _strlen(char *s)
+{
+char *c;
+int count = 0;
+
+*c = *s;
+while (*c != '\0')
+{
+count++;
+*c++;
+}
+_putchar (count);
+}
