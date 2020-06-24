0218a1342327276d98e11cc979480fe628c65354
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-24 02:12:55 +0000

    0x05.C

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - prints n elements of an array of integers
+ *@a: test variable
+ *@n: teste variable
+ *
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i; ir < n; i++)
+{
+printf("%d", a[i]);
+if (i != n)
+printf(", ");
+}
+printf("\n");
+}
