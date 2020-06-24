6f895bbe7f5aeea3794f202161ae4a6d6671ad5c
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-24 02:28:56 +0000

    0x05.C

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,9 @@
  */
 void print_array(int *a, int n)
 {
-int counter = 0;
+int counter;
 
-for (counter; counter < n; counter++)
+for (counter = 0; counter < n; counter++)
 {
 printf("%d", a[counter]);
 if (counter != n)
