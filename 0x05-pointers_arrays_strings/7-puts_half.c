130b4809148fd3e6639eb410712b3c8f2f4eecb6
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-24 01:14:25 +0000

    0x05.C

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *puts_half - prints half of a string
+ *@str: string to print
+ *
+ *Return: void
+ */
+void puts_half(char *str)
+{
+int i, count = 0, middle;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+count++;
+}
+middle = count / 2;
+if (count % 2 == 0)
+{
+for (i = middle; str[i] != 0; i++)
+{
+putchar(str[i]);
+}
+}
+else
+{
+middle = (count - 1) / 2;
+for (i = middle; str[i] != 0; i++)
+{
+putchar(str[i]);
+}
+}
+}
