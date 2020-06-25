d631a269c6b7db502244fc2e1db9049445e37090
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-25 16:11:12 +0000

    0x06.C

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: test variable
+ * @src: test variable
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
