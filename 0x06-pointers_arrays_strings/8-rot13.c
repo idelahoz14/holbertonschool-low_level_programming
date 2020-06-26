03bbc60dccbffcc8dc7fee90cff3b9cdb6d4901c
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-26 01:03:39 +0000

    0x06.C

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: test variable
+ * Return: s
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+			}
+		}
+	}
+	return (str);
+}
