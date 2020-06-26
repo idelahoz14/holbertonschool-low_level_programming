3202121b8a1855a607a3ccf4799d2ae70ba55f7d
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-26 22:59:23 +0000

    0x06.C

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,35 +14,23 @@ char *cap_string(char *str)
 		switch (str[i])
 		{
 		case ' ':
-			i++;
 		case '\t':
-			i++;
 		case '\n':
-			i++;
 		case ',':
-			i++;
 		case ';':
-			i++;
 		case '.':
-			i++;
 		case '!':
-			i++;
 		case '?':
-			i++;
 		case '"':
-			i++;
 		case '(':
-			i++;
 		case ')':
-			i++;
 		case '{':
-			i++;
 		case '}':
-			i++;
-
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
 		}
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] = str[0] - 32;
 	}
 	return (str);
 }
