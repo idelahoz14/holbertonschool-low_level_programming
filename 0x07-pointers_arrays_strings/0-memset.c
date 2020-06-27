2a185566f20c80e0122ad276c67a998996f49a11
Idelahoz <idelahzo14@cuc.edu.co>
2020-06-27 20:03:07 +0000

    0x06.C

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_memset - fills memory with constant byte
+ *@s: test variable
+ *@b: test variable
+ *@n: test variable
+ *
+ *Return: pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s= b;
+		s++;
+	}
+
+	return (s);
+}
