e51f91e965f871b154a8837a9d97d07ccd944652
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-01 18:02:36 +0000

    0x08.C

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
 /**
+ *_puts_recursion - prints a string recursively
  *
- *
- *
- *
+ *@s: test variable
+ *Return: void
  */
 
 void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		return (-1);
+	}
+
 	if (*s != '\0')
 	{
 		_putchar(*s);
 		s++;
 		_puts_rescursion(s);
-	}
-	else
-	{
-		return (-1);
 	}
 }
