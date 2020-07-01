46328e49e23c5a4b823a2ee83ec36870e33627e8
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-01 18:05:08 +0000

    0x08.C

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return (-1);
 	}
-
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-		_puts_rescursion(s);
-	}
+	_putchar(*s);
+	s++;
+        _puts_rescursion(s);
 }
