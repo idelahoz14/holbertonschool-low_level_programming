63d6a357a1a134a93a0da510e057313a7844553c
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-01 22:54:51 +0000

    0x08.C

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
 /**
- *is_prime_number - decides if integer is prime
- *@n: number to test
- *
- *Return: 1 if prime, 0 otherwise
- */
-int is_prime_number(int n)
-{
-	int test = 2;
-
-	return (test(n, test));
-}
-/**
  * check_num - check the number.
  * @num: number
  * @base: base number
- * Return: if num is prime 1
+ * Return: if is prime 1 else 0.
  */
-int test(int num, int base)
+int check_num(int num, int base)
 {
 	if (num <= 1 || num % base == 0)
 		return (0);
 	if (base * base > num)
 		return (1);
 	return (check_num(num, base + 1));
+}
+/**
+ * is_prime_number - check if a number is prime.
+ * @n: number
+ * Return: if is prime 1 else 0.
+ */
+int is_prime_number(int n)
+{
+	return (check_num(n, 2));
 }
