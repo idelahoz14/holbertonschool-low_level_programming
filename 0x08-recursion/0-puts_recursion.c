29133d10bc764bb784f3dfe934992a8695f9b4d9
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-01 18:32:35 +0000

    0x08.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string
@@ -14,6 +13,7 @@ void _puts_recursion(char *s)
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		s++;
+		_puts_recursion(s);
 	}
 }
