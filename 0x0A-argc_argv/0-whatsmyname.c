d18191a54c9c1a880aa5092b995c9cb7d79a1624
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-04 23:53:20 +0000

    0x0A.C

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number arguments
+ * @argv: array of string
+ * Return: Always 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
