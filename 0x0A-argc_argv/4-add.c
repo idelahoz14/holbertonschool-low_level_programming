300147c6b2f23ff4344e2bd76c0878a5c59e2301
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-05 22:48:15 +0000

    0x0A.C

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *manin - function
+ *@argc: number argument
+ *@argv: array of string
+ *Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i,j, result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result = result + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
