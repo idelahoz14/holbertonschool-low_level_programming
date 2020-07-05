5b444f5be95ea9e0a3f689cced4d8813f54f8591
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-05 23:12:14 +0000

    0x0A.C

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *main - functions
+ *@argc: number argument
+ *@argv: array of string
+ *Return: resul
  */
 int main(int argc, char *argv[])
 {
