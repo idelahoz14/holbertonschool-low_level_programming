5d4f253d6b0743b8e751f5e4f4767d97ad54d043
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-07 17:54:57 +0000

    0x0B.C

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates the array
+ * @size: size of the array
+ * @c: value of the array
+ * Return: pointer to the array, or NULL if
+ * it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(sizeof(char) * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0;i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	p[i] = '\0';
+	return (p);
+}
