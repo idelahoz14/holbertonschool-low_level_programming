7d9d25527b5be7cbbb65047badf405dfbca69c5b
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-07 18:25:30 +0000

    0x0B.C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,37 +8,31 @@
  */
 char *_strdup(char *str)
 {
-	int i;
+	char i, lengh = 0;
 	char *s;
 
-	s = malloc((sizeof(char) * _strlen(str)));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		lengh++;
+	}
+
+	s = malloc((sizeof(char) * lengh));
 
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < _strlen(str); i++)
+	for (i = 0; i < lengh; i++)
 	{
 		s[i] = str[i];
 	}
 
 	s[i] = '\0';
 	return (s);
-}
-
-/**
- * _strlen - returns the length of a string.
- * @s: Pointer
- * Return: Always 0.
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != 0; i++)
-	{
-	}
-
-	return (i);
 }
