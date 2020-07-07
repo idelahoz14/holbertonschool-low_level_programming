9dc54832f3a1ee1d82be15856c5a91d33f7c7de2
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-07 20:51:52 +0000

    0x0B.C

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int _strlen(char *str)
+{
+	int i, lengh;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		lengh++;
+	}
+
+	return (lengh);
+}
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: two strins or Null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, j, k;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	s = (char *)malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	k = 0;
+	for (i = 0; s1[i] != '\0'; i++, k++)
+	{
+		s[k] = s1[i];
+	}
+
+	for (j = 0; s2[j] != '\0'; j++, k++)
+	{
+		s[k] = s2[j];
+	}
+
+	s[k] = '\0';
+	return (s);
+}
