5b600c6457d3bb34816bc9d48032c0c359d928ab
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-14 00:46:26 +0000

    0x0E.C

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s\n", (*d).name);
+		printf("Name: %s\n", d->name);
 	}
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
@@ -26,6 +26,6 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Owner: %s\n", (*d).owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
