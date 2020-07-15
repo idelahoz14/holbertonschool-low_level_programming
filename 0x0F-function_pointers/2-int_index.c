1008b1856a912828aed4656fa003c50c9f4f4690
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-15 23:05:23 +0000

    0x0F.C

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * int_index - searches for an integer
+ * @array: array with data
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: count of integer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
