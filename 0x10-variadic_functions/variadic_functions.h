5f1bb521a003c0077a8f36f710f890599161b412
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-19 01:05:43 +0000

    0x10.C

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,18 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct p - checker struct
+ * @p: possibility
+ * @f: Function to be used
+ */
+typedef struct struc
+{
+	char *p;
+	void (*f)(va_list);
+} stru;
 
 #endif
