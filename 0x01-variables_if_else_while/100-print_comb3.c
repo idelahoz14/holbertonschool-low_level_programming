7c65dda5e1ea31c81f6c5023c15a8e365102c722
idelahoz14 <idelahzo14@cuc.edu.co>
2020-08-19 13:25:33 +0000

    0x01.C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48 ; a <= 57 ; a++)
+	{
+		for (b = 48 ; b <= 57 ; b++)
+		{
+			if (a == b)
+			{
+				continue;
+			}
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 57 && b == 57)
+				{
+					break;
+				}
+				if (a == 56 && b == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
